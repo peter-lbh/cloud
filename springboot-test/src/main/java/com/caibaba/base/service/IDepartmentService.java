8405b803173709941f3a3e799228ab4420d50fb9
peter <lbhlivebetter@163.com>
2019-07-28 16:24:34 +0800

    删除了

--- springboot-test/src/main/java/com/caibaba/base/service/IDepartmentService.java
+++
@@ -1,9 +0,0 @@
-package com.caibaba.base.service;
-
-import com.caibaba.base.entitys.Department;
-
-import java.util.List;
-
-public interface IDepartmentService {
-    List<Department> selectAll();
-}
